fb9944e3bd9f6b28153efa9c104a41c606691964
Ragnar <ragnar.naruski@voco.ee>
2023-03-27 19:17:21 +0300

    makse front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react"
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [paymentLink, setPaymentLink] = useState(null);
+    const [sum, setSum] = useState('');
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:3006/jrk/${jrk}`)
+    //         .then(res => res.json())
+    //         .then(json => setToode(json));
+    // }, []);
+    // const item = async (event) => {
+    //     event.preventDefault()
+    //     try {
+    //         const response = await axios.get(`http://localhost:3006/jrk/${jrk}`)
+    //         setToode(response.data)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // };
+
+    const ylekanne = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await axios.get(`http://localhost:3006/payment/${sum}`)
+            setPaymentLink(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={ylekanne}>
+                <label>Üle kantav summa:</label><br/>
+                <input type="number" value={sum} onChange={(event) => setSum(event.target.value)}/><br/>
+                <button type='submit'>Maksa</button>
+            </form>
+            {paymentLink && (<a href={paymentLink} target="_blank">Ülekande link</a>)}
+        </div>
+    )
 }
 
 export default App;
